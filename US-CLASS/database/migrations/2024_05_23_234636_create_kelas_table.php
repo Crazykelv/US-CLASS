f8831914214f96c7e925743c47046b34ea042390
Fahri Aminuddin Abdillah <fahriaminnudin@gmail.com>
2024-05-24 06:53:47 +0700

    First Laravel Setup

---
+++ US-CLASS/database/migrations/2024_05_23_234636_create_kelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('namaRuangan');
+            $table->string('lokasi')->nullable();
+            $table->string('fasilitas')->nullable();
+            $table->string('usedBy')->nullable();
+            $table->string('usedFor')->nullable();
+            $table->smallInteger('ketersediaan')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
